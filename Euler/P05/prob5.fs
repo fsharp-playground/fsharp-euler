3d222c74ed9474faf6fd66558c9a32881498bbeb
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-06-08 18:55:01 +0700

    reformat comment

--- Euler/P05/prob5.fs
+++ Euler/P05/prob5.fs
@@ -1,13 +1,18 @@
-///Project Euler Problem 5
-///2520 is the smallest number that can be divided by each of the numbers 
-///from 1 to 10 without any remainder.
-///
-///What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
-// This solution came from here: http://geekswithblogs.net/Erik/archive/2008/02/19/119734.aspx
+
+(*
+Project Euler Problem 5
+2520 is the smallest number that can be divided by each of the numbers 
+from 1 to 10 without any remainder.
+
+What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
+This solution came from here: http://geekswithblogs.net/Erik/archive/2008/02/19/119734.aspx *)
+
 open System
 
-// The function primeFactors has been a workhorse for solving other euler problems.
-// Thanks Erik.
+(*
+The function primeFactors has been a workhorse for solving other euler problems.
+Thanks Erik. *)
+
 let primeFactors (num:float) =
     let get num2  =
         let sq = Math.Sqrt (num2)
--- Euler/P06/squares.fs
+++ Euler/P06/squares.fs
@@ -1,20 +1,23 @@
-///Project Euler Problem 6
-///The sum of the squares of the first ten natural numbers is,
-///                 1^2 + 2^2 + ... + 10^2 = 385
-///
-///The square of the sum of the first ten natural numbers is,
-///                (1 + 2 + ... + 10)^2 = 552 = 3025
-///
-///Hence the difference between the sum of the squares of the first ten natural 
-///numbers and the square of the sum is 3025 385 = 2640.
-///
-///Find the difference between the sum of the squares of the 
-///first one hundred natural numbers and the square of the sum.
-//-----------------------------------------------------------------------
-// <copyright file="squares.fs" >
-//     Copyright © Cesar Mendoza. All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
+(*
+Project Euler Problem 6
+The sum of the squares of the first ten natural numbers is,
+                 1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+                (1 + 2 + ... + 10)^2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural 
+numbers and the square of the sum is 3025 385 = 2640.
+
+Find the difference between the sum of the squares of the 
+first one hundred natural numbers and the square of the sum.
+
+-----------------------------------------------------------------------
+<copyright file="squares.fs" >
+     Copyright © Cesar Mendoza. All rights reserved.
+</copyright>
+-----------------------------------------------------------------------
+*)
 open System
 
 let limit = 100
